8fae7677c329764d6a26c6e3aad7a801507c5b4a
justhsmm <119434491+justhsmm@users.noreply.github.com>
2022-12-15 23:40:28 +0700

    Create ui-modal-notification.demo.js

---
+++ ui-modal-notification.demo.js
@@ -0,0 +1,21 @@
+ 
+var handleToastsInit = function() {
+	$('[data-init="toast"]').toast('show');
+};
+
+var handleToastToggle = function() {
+	$(document).on('click', '[data-toggle="toast"]', function(e) {
+		e.preventDefault();
+		
+		var targetElm = $(this).attr('data-target');
+		$(targetElm).toast('show');
+	});
+};
+
+
+/* Controller
+------------------------------------------------ */
+$(document).ready(function() {
+	handleToastsInit();
+	handleToastToggle();
+});
